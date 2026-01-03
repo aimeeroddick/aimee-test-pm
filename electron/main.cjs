643abb4805a299c6180def9145cae43dad56d5fc
aimeeroddick <aimee.roddick@spicymango.co.uk>
2026-01-03 13:18:21 +0000

    Fix Electron module format - rename to .cjs for CommonJS compatibility

--- electron/main.cjs
+++ electron/main.cjs
@@ -23,7 +23,7 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.cjs')
     },
     icon: path.join(__dirname, '../public/icon.png'),
     show: false // Don't show until ready
