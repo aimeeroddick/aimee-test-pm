dbb5fe7399dd36cd98af2d7d56853b204aac297f
aimeeroddick <aimee.roddick@spicymango.co.uk>
2026-01-08 09:58:02 +0000

    Fix generate-pngs script to use sips, regenerate all icons

--- public/generate-pngs.sh
+++ public/generate-pngs.sh
@@ -6,26 +6,22 @@ cd "$(dirname "$0")"
 
 echo "Generating PNG files from SVGs..."
 
-# Favicon icons - use favicon.svg (optimized for small sizes, transparent)
+# Favicon icons - use sips for proper PNG generation from favicon.svg
 for size in 16 32 64 80 128; do
-  qlmanage -t -s $size -o . favicon.svg 2>/dev/null
-  mv favicon.svg.png icon-$size.png 2>/dev/null
+  sips -s format png -z $size $size favicon.svg --out icon-$size.png 2>/dev/null
   echo "Created icon-$size.png from favicon.svg"
 done
 
 # logo.png (56x56) - transparent, use logo.svg
-qlmanage -t -s 56 -o . logo.svg 2>/dev/null
-mv logo.svg.png logo.png 2>/dev/null
+sips -s format png -z 56 56 logo.svg --out logo.png 2>/dev/null
 echo "Created logo.png"
 
 # logo-full.png (200 width)
-qlmanage -t -s 200 -o . logo-full.svg 2>/dev/null
-mv logo-full.svg.png logo-full.png 2>/dev/null
+sips -s format png -z 200 200 logo-full.svg --out logo-full.png 2>/dev/null
 echo "Created logo-full.png"
 
 # apple-touch-icon (180x180) - USE PWA VERSION WITH BACKGROUND
-qlmanage -t -s 180 -o . logo-pwa.svg 2>/dev/null
-mv logo-pwa.svg.png apple-touch-icon.png 2>/dev/null
+sips -s format png -z 180 180 logo-pwa.svg --out apple-touch-icon.png 2>/dev/null
 echo "Created apple-touch-icon.png (with background)"
 
 # PWA icons (in /icons/ folder) - USE PWA VERSION WITH BACKGROUND
@@ -34,8 +30,7 @@ echo "Generating PWA icons with background..."
 mkdir -p icons
 
 for size in 72 96 128 144 152 192 384 512; do
-  qlmanage -t -s $size -o . logo-pwa.svg 2>/dev/null
-  mv logo-pwa.svg.png icons/icon-${size}x${size}.png 2>/dev/null
+  sips -s format png -z $size $size logo-pwa.svg --out icons/icon-${size}x${size}.png 2>/dev/null
   echo "Created icons/icon-${size}x${size}.png"
 done
 
